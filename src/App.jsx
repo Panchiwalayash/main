654802744579e1a32feedd3ad86d897939784c6b
Yash Panchiwala <87870272+Panchiwalayash@users.noreply.github.com>
2023-01-31 13:18:05 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+// versi "react-qr-reader" 1.0.0. component API harus disesuaikan dengan yg baru
+import { useState } from "react";
+import {QrReader} from "react-qr-reader";
+
+const App = () => {
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
+
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
+    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
+  return (
+    <div className="App">
+      <h1>Hello user</h1>
+
+      <button
+        onClick={() => {
+          setStartScan(!startScan);
+        }}
+      >
+        {startScan ? "Stop Scan" : "Start Scan"}
+      </button>
+      {startScan && (
+        <>
+        <h1>Scanning started...</h1>
+          <QrReader
+            delay={50}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "30px" }}
+          />
+        </>
+      )}
+      {loadingScan && <p>Loading</p>}
+      {data !== "" && <p>{data}</p>}
+    </div>
+  );
+};
+
+export default App;
